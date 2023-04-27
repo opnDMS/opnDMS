ee25afbac974e13ae63f011ea4014ef10a5400f6
Rose Reed <rose@rosedev.de>
2023-04-27 19:00:14 +0200

    first try on fixing #22 very buggy

--- upload/style.js
+++ upload/style.js
@@ -4,7 +4,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     fileUploadStyling(single);
 
-})
+    removeSelectBottomBorders();
+});
 
 function fileUploadStyling(single){
     let inputFile = $(":file");
@@ -21,3 +22,47 @@ function fileUploadStyling(single){
             }
         });
     };
+
+
+/* <select> styling */
+
+function removeSelectBottomBorders(){
+    //add html event listener
+    $("html").on("click keydown touchstart", (e) => {
+        let tmpSelectItem = selectItem;
+        
+        if (e.target.tagName != "OPTION" && e.target.tagName != "SELECT") {
+            //$("html").trigger("focus");
+            console.debug("CLICK OUTSIDE OF SELECT!");
+            if ($("select").hasClass("bottom-flat")) {
+                $("select").removeClass("bottom-flat");
+                $("html").unbind("click keydown touchstart");
+            }
+        }
+    });
+    //select all <select> tags
+    let input = $("select");
+    console.log(input.length);
+    //cycle through all selected select tags
+    for (let i = 0; i < input.length; i++) {
+        var selectItem = input[i];
+        $(selectItem).on("click keydown touchstart", (e) => {
+            if (e.target.tagName == "SELECT") {
+                if (!$(e.target).hasClass("bottom-flat")) {
+                    $(e.target).addClass("bottom-flat");
+                } else if ($(e.target).hasClass("bottom-flat")) {
+                    $(e.target).removeClass("bottom-flat");
+                    $("html").unbind("click keydown touchstart");
+                }
+            } else if (e.target.tagName == "OPTION") {
+                if ($(e.target).parent().hasClass("bottom-flat")) {
+                    $(e.target).parent().removeClass("bottom-flat");
+                    $("html").unbind("click keydown touchstart");
+                };
+            } else {
+                //$("html").unbind("click keydown touchstart");
+                console.warn("last Else triggered")
+            };
+            });
+}};
+
